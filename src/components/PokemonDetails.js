65e2e2e737bd80a1971390d804c2dc3751350b63
Benjamin Klein <klein.benjaminj@gmail.com>
2024-01-20 20:57:54 -0700

    modal styling

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
 import { Bar } from "react-chartjs-2";
 import "chart.js/auto";
+import "chartjs-plugin-datalabels";
 
 const PokemonDetails = ({ pokemonId, showModal, handleClose }) => {
   const [pokemonDetails, setPokemonDetails] = useState(null);
 
   const cleanDescription = (description) => {
-    // Remove line breaks and extra spaces
     return description.replace(/\n|\r/g, " ").replace(/\s+/g, " ").trim();
+  };
+
+  const capitalizeFirstLetter = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1);
   };
 
   useEffect(() => {
@@ -19,17 +23,14 @@ const PokemonDetails = ({ pokemonId, showModal, handleClose }) => {
         );
         const data = await response.json();
 
-        // Fetch additional details including species
         const speciesResponse = await fetch(data.species.url);
         const speciesData = await speciesResponse.json();
 
-        // Filter flavor text entries for English language and "red" version
         const redEntries = speciesData.flavor_text_entries.filter(
           (entry) =>
             entry.language.name === "en" && entry.version.name === "red"
         );
 
-        // Use the first entry (you can customize this logic)
         const flavorText =
           redEntries.length > 0
             ? cleanDescription(redEntries[0].flavor_text)
@@ -56,138 +57,116 @@ const PokemonDetails = ({ pokemonId, showModal, handleClose }) => {
   return (
     <Modal show={showModal} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>{pokemonDetails.name}'s Details</Modal.Title>
+        <Modal.Title>
+          {capitalizeFirstLetter(pokemonDetails.name)}'s Details
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Modal.Body>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <img
             src={pokemonDetails.sprites.front_default}
             alt={pokemonDetails.name}
             onLoad={() => window.dispatchEvent(new Event("resize"))}
+            style={{ width: "100%", marginBottom: "10px" }}
           />
 
-          <Bar
-            data={{
-              labels: [
-                "HP",
-                "Attack",
-                "Defense",
-                "Special Attack",
-                "Special Defense",
-                "Speed",
-              ],
-              datasets: [
-                {
-                  label: "Base Stat",
-                  data: [
-                    pokemonDetails.stats.find((stat) => stat.stat.name === "hp")
-                      ?.base_stat || 0,
-                    pokemonDetails.stats.find(
-                      (stat) => stat.stat.name === "attack"
-                    )?.base_stat || 0,
-                    pokemonDetails.stats.find(
-                      (stat) => stat.stat.name === "defense"
-                    )?.base_stat || 0,
-                    pokemonDetails.stats.find(
-                      (stat) => stat.stat.name === "special-attack"
-                    )?.base_stat || 0,
-                    pokemonDetails.stats.find(
-                      (stat) => stat.stat.name === "special-defense"
-                    )?.base_stat || 0,
-                    pokemonDetails.stats.find(
-                      (stat) => stat.stat.name === "speed"
-                    )?.base_stat || 0,
-                  ],
-                  backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(255, 206, 86, 0.2)",
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(153, 102, 255, 0.2)",
-                    "rgba(255, 159, 64, 0.2)",
-                  ],
-                  borderColor: [
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(54, 162, 235, 1)",
-                    "rgba(255, 206, 86, 1)",
-                    "rgba(75, 192, 192, 1)",
-                    "rgba(153, 102, 255, 1)",
-                    "rgba(255, 159, 64, 1)",
-                  ],
-                  borderWidth: 1,
-                },
-              ],
+          <div
+            style={{
+              width: "100%",
+              border: "1px solid #ccc",
+              padding: "10px",
+              marginBottom: "10px",
             }}
-            options={{
-              indexAxis: "y", // Use 'y' to make it a horizontal bar chart
-              scales: {
-                x: {
-                  beginAtZero: true,
-                  max: 150, // Adjust the max value based on your data
-                },
-              },
-              plugins: {
-                legend: {
-                  display: false,
-                },
-                tooltip: {
-                  callbacks: {
-                    label: (context) => `${context.label}: ${context.parsed.y}`,
+          >
+            <Bar
+              data={{
+                labels: [
+                  "HP",
+                  "Attack",
+                  "Defense",
+                  "Special Attack",
+                  "Special Defense",
+                  "Speed",
+                ],
+                datasets: [
+                  {
+                    label: "Base Stat",
+                    data: [
+                      pokemonDetails.stats.find(
+                        (stat) => stat.stat.name === "hp"
+                      )?.base_stat || 0,
+                      pokemonDetails.stats.find(
+                        (stat) => stat.stat.name === "attack"
+                      )?.base_stat || 0,
+                      pokemonDetails.stats.find(
+                        (stat) => stat.stat.name === "defense"
+                      )?.base_stat || 0,
+                      pokemonDetails.stats.find(
+                        (stat) => stat.stat.name === "special-attack"
+                      )?.base_stat || 0,
+                      pokemonDetails.stats.find(
+                        (stat) => stat.stat.name === "special-defense"
+                      )?.base_stat || 0,
+                      pokemonDetails.stats.find(
+                        (stat) => stat.stat.name === "speed"
+                      )?.base_stat || 0,
+                    ],
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.2)",
+                      "rgba(54, 162, 235, 0.2)",
+                      "rgba(255, 206, 86, 0.2)",
+                      "rgba(75, 192, 192, 0.2)",
+                      "rgba(153, 102, 255, 0.2)",
+                      "rgba(255, 159, 64, 0.2)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                      "rgba(255, 206, 86, 1)",
+                      "rgba(75, 192, 192, 1)",
+                      "rgba(153, 102, 255, 1)",
+                      "rgba(255, 159, 64, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+              options={{
+                indexAxis: "y",
+                scales: {
+                  x: {
+                    beginAtZero: true,
+                    max: 150,
                   },
                 },
-              },
-            }}
-          />
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                  datalabels: {
+                    anchor: "end",
+                    align: "end",
+                    color: "black",
+                    font: {
+                      weight: "bold",
+                    },
+                    formatter: (value, context) =>
+                      context.dataset.label + ": " + value,
+                  },
+                  tooltips: {
+                    enabled: false,
+                  },
+                },
+              }}
+            />
+          </div>
 
-          {/* Other details... */}
-        </Modal.Body>
-
-        <p>
-          HP:{" "}
-          {
-            pokemonDetails.stats.find((stat) => stat.stat.name === "hp")
-              ?.base_stat
-          }
-        </p>
-        <p>
-          Attack:{" "}
-          {
-            pokemonDetails.stats.find((stat) => stat.stat.name === "attack")
-              ?.base_stat
-          }
-        </p>
-        <p>
-          Defense:{" "}
-          {
-            pokemonDetails.stats.find((stat) => stat.stat.name === "defense")
-              ?.base_stat
-          }
-        </p>
-        <p>
-          Special Attack:{" "}
-          {
-            pokemonDetails.stats.find(
-              (stat) => stat.stat.name === "special-attack"
-            )?.base_stat
-          }
-        </p>
-        <p>
-          Special Defense:{" "}
-          {
-            pokemonDetails.stats.find(
-              (stat) => stat.stat.name === "special-defense"
-            )?.base_stat
-          }
-        </p>
-        <p>
-          Speed:{" "}
-          {
-            pokemonDetails.stats.find((stat) => stat.stat.name === "speed")
-              ?.base_stat
-          }
-        </p>
-        {/* Add other stats as needed */}
-        <p>Description: {pokemonDetails.species.flavor_text}</p>
+          <div
+            style={{ width: "100%", border: "1px solid #ccc", padding: "10px" }}
+          >
+            <p>Description: {pokemonDetails.species.flavor_text}</p>
+          </div>
+        </div>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
